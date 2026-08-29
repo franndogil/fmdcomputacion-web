adb108ea311be1d3d6538c6196e5a9100032323f
franndogil <franndogil@gmail.com>
2026-08-29 15:45:00 -0300

    correccion de botones

--- js/nav.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    document.getElementById('main-nav').classList.toggle('menu-open');
-}
-function closeMenu() {
-    document.getElementById('main-nav').classList.remove('menu-open');
-}
